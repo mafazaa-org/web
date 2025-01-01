ad3afadd9098beab2c229f642cbf85980287230a
Ahmed Elbehairy <ahmedelbehairy.72005@gmail.com>
2025-01-01 13:55:46 +0200

    removed poem section

--- app/page.tsx
+++ app/page.tsx
@@ -6,12 +6,9 @@ import homeData from "./homeData";
 import FollowUs from "@/components/FollowUs";
 import Form from "@/components/Form";
 import { useEffect, useState } from "react";
-import PoemSection, { poemType } from "@/components/poemSection";
 
 export default function Home() {
 	const [projects, setProjects] = useState([]);
-	const [poem, setPoem] = useState<poemType>();
-	const [loaded, setLoaded] = useState(false);
 	useEffect(() => {
 		(async () => {
 			const api_host = await (
@@ -22,20 +19,15 @@ export default function Home() {
 				await fetch(`${api_host}/global/projects`)
 			).json();
 			setProjects(res);
-			const p = await (await fetch(`${api_host}/global/poem`)).json();
-			setPoem(p);
-			setLoaded(true);
 		})();
 	}, []);
 	return (
-		loaded && (
-			<main dir="rtl">
-				<PoemSection {...(poem as poemType)} />
-				{homeData.map((element, index) => (
-					<Section {...element} key={index} />
-				))}
-				<section
-					className="px-4 md:px-10 lg:px-32 py-10 relative 
+		<main dir="rtl">
+			{homeData.map((element, index) => (
+				<Section {...element} key={index} />
+			))}
+			<section
+				className="px-4 md:px-10 lg:px-32 py-10 relative 
           after:content-['']
           after:absolute
           after:left-1/4
@@ -44,20 +36,19 @@ export default function Home() {
           after:h-[0.5px]
           after:block
         after:bg-black"
-					id="projects"
-				>
-					<h1 className="font-bold text-2xl lg:text-5xl mb-6">
-						مشاريعنا
-					</h1>
-					<div className="grid md:grid-cols-3 gap-8 ">
-						{projects.map((element: any, index) => (
-							<Project {...element} key={index} />
-						))}
-					</div>
-				</section>
-				<FollowUs />
-				<Form />
-			</main>
-		)
+				id="projects"
+			>
+				<h1 className="font-bold text-2xl lg:text-5xl mb-6">
+					مشاريعنا
+				</h1>
+				<div className="grid md:grid-cols-3 gap-8 ">
+					{projects.map((element: any, index) => (
+						<Project {...element} key={index} />
+					))}
+				</div>
+			</section>
+			<FollowUs />
+			<Form />
+		</main>
 	);
 }
