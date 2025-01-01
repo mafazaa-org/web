0533151b10237628659d4545e406f345249c896a
Ahmed Elbehairy <ahmedelbehairy.72005@gmail.com>
2025-01-01 04:15:23 +0200

    added headers

--- app/api/public_env/api_host/route.ts
+++ app/api/public_env/api_host/route.ts
@@ -1,5 +1,18 @@
 import { NextRequest, NextResponse } from "next/server";
 
+const resInit = {
+	headers: {
+		"Access-Control-Allow-Origin": "*",
+		"Access-Control-Allow-Methods": "GET, OPTIONS",
+		"Access-Control-Allow-Headers":
+			"Access-Control-Allow-Origin, Access-Control-Allow-Methods, Content-Type, Authorization",
+	},
+};
+
 export async function GET(req: NextRequest) {
-	return NextResponse.json(process.env.API_HOST);
+	return NextResponse.json(process.env.API_HOST, resInit);
+}
+
+export async function OPTIONS(req: NextRequest) {
+	return NextResponse.json("", resInit);
 }
