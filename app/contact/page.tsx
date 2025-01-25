ed61aa148e96ad57e7fc683fcdf0dee3b2b934e2
youssefzaki00 <107646038+youssefzaki00@users.noreply.github.com>
2025-01-25 13:52:38 +0200

    fix error

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,20 +1,20 @@
-import Form from "@/components/Form";
+import { Form } from "mafazaa-react-ui";
 import { Metadata } from "next";
 import React from "react";
 
 export const metadata: Metadata = {
-	title: "مفازا - تواصل معنا",
+  title: "مفازا - تواصل معنا",
 };
 
 function ContactPage() {
-	return (
-		<div
-			dir="rtl"
-			className="bg-gradient-to-r from-dark-one to-dark-two h-full w-full text-white mt-24"
-		>
-			<Form />
-		</div>
-	);
+  return (
+    <div
+      dir="rtl"
+      className="bg-gradient-to-r from-dark-one to-dark-two h-full w-full text-white mt-24"
+    >
+      <Form />
+    </div>
+  );
 }
 
 export default ContactPage;
