186dc3f0ea1deffc33a0f2bd1c675f4fe808172b
‪Ahmed Elbehairy‬‏ <ahmedelbehairy.72005@gmail.com>
2025-03-14 17:23:44 +0200

    fixed important stuff

--- app/support_us/page.tsx
+++ app/support_us/page.tsx
@@ -10,15 +10,6 @@ const description = `مفازا هو مشروع دعوي إسلامي يهدف �
 const SupportUsPage = () => {
 	return (
 		<main className="min-h-screen bg-gradient-to-b from-bright-one to-gray-100 flex flex-col">
-			{/* Hero Section */}
-			<CenterSection
-				title="ادعم مفازا"
-				description={description}
-				backgroundImage={fundImage}
-				buttons={[]}
-				textColor="white"
-			/>
-
 			{/* Form Section */}
 			<section className="flex items-center justify-center md:px-6 md:py-16 lg:py-20">
 				<div className="w-full md:w-full max-w-lg bg-white p-4 md:p-10 md:rounded-3xl md:shadow-2xl border border-gray-200">
@@ -46,6 +37,7 @@ const SupportUsPage = () => {
 								className="w-full px-5 py-3 border border-gray-300 rounded-lg bg-gray-100 text-black 
   font-medium transition duration-200 focus:bg-secondary-color/25 outline-none"
 								min="10"
+								required
 								step="any"
 							/>
 							{/* {errors.amount && (
