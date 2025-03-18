016c8f7e9881a11967fce4a1ed56a6c0151a3de8
‪Ahmed Elbehairy‬‏ <138185767+ahmed-elbehairy7@users.noreply.github.com>
2025-03-18 15:43:33 +0200

    Update qr link

--- app/qr_link/route.ts
+++ app/qr_link/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
-	return NextResponse.redirect("https://ainaa.mafazaa.com");
+	return NextResponse.redirect("https://whatsapp.com/channel/0029Vb5NRuqLY6dCU3qeXO2i/100");
 }
